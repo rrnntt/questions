be56b877eb96101da369378ae01974819d31d3d5
rrnntt <rrnntt@ubuntu>
2013-05-01 16:06:24 +0100

    Fixed issue with inline math with markdown

--- markdown_postprocess.py
+++ markdown_postprocess.py
@@ -1,6 +1,9 @@
 import re
 
 youtube_pattern = re.compile('&lt;iframe(.*?)src="http://www\.youtube\.com/embed/(.*?)&gt;&lt;/iframe&gt;')
+inline_math_pattern = re.compile('\$(.*?)\$')
 
 def postprocess(html):
-    return re.sub(youtube_pattern,'<iframe \g<1> src="http://www.youtube.com/embed/\g<2>></iframe>',html)
+    tmp = re.sub(youtube_pattern,'<iframe \g<1> src="http://www.youtube.com/embed/\g<2>></iframe>',html)
+    tmp = re.sub(inline_math_pattern,'\\(\g<1>\\)',tmp)
+    return tmp
