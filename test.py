c0821abe8c0a388a7f90e00badf1f0b812f13e66
rrnntt <rrnntt@ubuntu>
2013-05-01 16:21:57 +0100

    Fixed display math with markdown

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ world
 
 $E=mc^2>0$
 
+$$E=mc^2>0$$
+
 <iframe width="560" height="315" src="http://www.youtube.com/embed/zA92Rw6kNWw" frameborder="0" allowfullscreen></iframe>
 <script type="text/javascript" src="/jquery/js/jquery-ui-1.9.2.custom.min.js"></script>
 
